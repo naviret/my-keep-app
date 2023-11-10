72ac5b5a613b347b6711bc1cc48f07d4ae3c9143
naviret <111366578+naviret@users.noreply.github.com>
2023-11-10 09:19:44 -0500

    setup with prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,21 @@
-import React from "react";
-import { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from  "./Note";
+import React from 'react';
+import { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
 
 const date = new Date();
 const year = date.getFullYear();
 
 function App() {
     return (
-        <div>        
+        <div>
             <Header text="Keeper"></Header>
             <Footer year={year}></Footer>
-            <Note 
-                title="Hello World" 
-                content="saying hi"
-            ></Note>
-            
+            <Note title="Hello World" content="saying hi"></Note>
+            <Note title="Hello World" content="saying hi"></Note>
         </div>
-    )
+    );
 }
 
 export default App;
