536e7c9030d8e8511a32eabb47b5ffba7e84aa12
naviret <111366578+naviret@users.noreply.github.com>
2023-11-10 10:02:33 -0500

    completed form styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,26 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 
-const date = new Date();
-const year = date.getFullYear();
-
 function App() {
     return (
         <div>
-            <Header text="Keeper"></Header>
-            <Footer year={year}></Footer>
-            <Note title="Hello World" content="saying hi"></Note>
-            <Note title="Hello World" content="saying hi"></Note>
+            <Header text='Keeper'></Header>
+            <Footer></Footer>
+
+            <div className='add-note'>
+                <form>
+                    <input name='Title' placeholder='Title'></input>
+                    <textarea
+                        name='Content'
+                        placeholder='Take a note...'
+                        rows='3'
+                    ></textarea>
+                    <button>Add</button>
+                </form>
+            </div>
+
+            <Note title='Hello World' content='saying hi'></Note>
+            <Note title='Hello World' content='saying hi'></Note>
         </div>
     );
 }
