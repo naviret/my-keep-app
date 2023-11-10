3eb3ab007e9e637bde468587a68f4f9f9d9156de
naviret <111366578+naviret@users.noreply.github.com>
2023-11-10 08:48:09 -0500

    created and tested Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
+const date = new Date();
+const year = date.getFullYear();
 
 function App() {
     return (
         <div>        
             <Header text="Keeper"></Header>
+            <Footer year={year}></Footer>
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,14 @@
 import React from "react";
-import React, { useState } from "react";
+import { useState } from "react";
+
+function Footer(props) {
+    return (
+        <footer>
+            <p> 
+                Copyright {props.year}
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
